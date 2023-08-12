c0e6586a94ea5ef3b8a7f3ea67488344160b8e22
Arturo Quiroz <r2inlondon@gmail.com>
2023-08-12 15:20:59 +0100

    feat : all routes completed for cocktail model

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -17,16 +17,49 @@ module Api
         cocktail = Cocktail.new(cocktail_params)
 
         if cocktail.save
-          render json: { status: 'SUCCESS', message: 'Saved cocktail', data: cocktail }, status: :ok  
+          render json: { status: 'SUCCESS', message: 'Saved cocktail', data: cocktail }, status: :ok
         else
-          render json: { status: 'ERROR', message: 'Cocktail not saved', data: cocktail.errors }, status: :unprocessable_entity 
+          render json: { status: 'ERROR', message: 'Cocktail not saved', data: cocktail.errors },
+                 status: :unprocessable_entity
         end
       end
-      
+
+      def show
+        cocktail = Cocktail.find(params[:id])
+
+        if cocktail
+          render json: { status: 'SUCCESS', message: 'Loaded cocktail', data: cocktail }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Cocktail not found' }, status: :not_found
+        end
+      end
+
+      def update
+        cocktail = Cocktail.find(params[:id])
+
+        if cocktail.update(cocktail_params)
+          render json: { status: 'SUCCESS', message: 'Updated cocktail', data: cocktail }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Cocktail not updated', data: cocktail.errors }
+        end
+      end
+
+      def destroy
+        cocktail = Cocktail.find(params[:id])
+
+        if cocktail.destroy
+          render json: { status: 'SUCCESS', message: 'Deleted cocktail', data: cocktail }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Cocktail not deleted', data: cocktail.errors }
+        end
+      end
+
       private
 
       def cocktail_params
-        params.require(:cocktail).permit(:name, :image )
+        params.require(:cocktail).permit(:name, :image).tap do |whitelisted|
+          whitelisted[:name] = params[:cocktail][:name].strip
+        end
       end
     end
   end
