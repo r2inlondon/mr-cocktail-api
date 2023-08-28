c2344efe972e8530368123efb798b4e2625bdc5a
Arturo Quiroz <r2inlondon@gmail.com>
2023-08-28 21:29:50 +0100

    feat : index cocktail route is now including the ingredients associated to each cocktail

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -2,12 +2,25 @@ module Api
   module V1
     class CocktailsController < ApplicationController
       def index
-        # Your logic for fetching cocktails goes here
-        # For example:
         cocktails = Cocktail.all
 
+        newCocktails = cocktails.map do |cocktail|
+          cocktail_ingredients = cocktail.ingredients.map do |ingredient|
+            { id: ingredient.id, description: ingredient.description, cocktail_id: ingredient.cocktail_id }
+          end
+
+          # cocktail_ingredients = Cocktail.find(cocktail.id).ingredients
+
+          {
+            id: cocktail.id,
+            name: cocktail.name,
+            image: cocktail.image,
+            ingredients: cocktail_ingredients
+          }
+        end
+
         if cocktails
-          render json: { status: 'SUCCESS', message: 'Loaded cocktails', data: cocktails }, status: :ok
+          render json: { status: 'SUCCESS', message: 'Loaded cocktails', data: newCocktails }, status: :ok
         else
           render json: { status: 'ERROR', message: 'Cocktails not found', data: cocktails }, status: :not_found
         end
@@ -58,7 +71,7 @@ module Api
 
       def cocktail_params
         params.require(:cocktail).permit(:name, :image).tap do |element|
-          element[:name] = params[:cocktail][:name].strip.capitalize()
+          element[:name] = params[:cocktail][:name].strip.capitalize
         end
       end
     end
